e8ed3b1cdb7c5547fef74cc4dd9e050e9c1ec5e5
Bee <breenicelee@gmail.com>
2025-02-24 20:38:40 -0500

    jumping physics

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -18,20 +18,28 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float moveX = 0f;
+        float moveY = 0f;
         float moveZ = 0f;
 
         if (Input.GetKey(KeyCode.UpArrow)) moveZ = -1f;
         if (Input.GetKey(KeyCode.DownArrow)) moveZ = 1f;
         if (Input.GetKey(KeyCode.LeftArrow)) moveX = 1f;
         if (Input.GetKey(KeyCode.RightArrow)) moveX = -1f;
+        
+        // jump
+        if (Input.GetKey(KeyCode.Space) && transform.position.y < 2f)
+        {
+            moveY = 1f;
+        }
 
-        moveDirection = new Vector3(moveX, 0f, moveZ).normalized;
+        moveDirection = new Vector3(moveX, moveY, moveZ).normalized;
 
         // walk
         rb.linearVelocity = moveDirection * moveSpeed;
+        rb.AddForce(Vector3.down * 9.81f * 2f, ForceMode.Acceleration); // bigger gravity acceleration
 
         // turn
-        if (moveDirection != Vector3.zero)
+        if ((moveDirection != Vector3.zero) && (!Input.GetKey(KeyCode.Space)))
         {
             Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
             transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, Time.deltaTime * 10f);
